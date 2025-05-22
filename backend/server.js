238ca16ed2ca9648f08e613ac7b52a41d2999119
Gabriel Valenzona <gabrielvalenzona6@gmail.com>
2025-05-22 18:26:49 -0500

    playing with routing

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv").config();
 
 app.get('/', (req,res) => {
     res.send();
 });
 
-app.listen(5000, () =>{
-    console.log("Server running on port 5000!");
+app.get('/api/contacts', (req,res) => {
+    res.json({message: "Get all contacts"});
+});
+
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Server running on port ${process.env.PORT}!`);
 });
 
 console.log("booby");
