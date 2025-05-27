f35f7f380f95a35225458cb04e06682e09540fed
Gabriel Valenzona <gabrielvalenzona6@gmail.com>
2025-05-27 14:21:54 -0500

    added if-statement in POST to ensure all fields in body are present

--- backend/controllers/controllerContact.js
+++ backend/controllers/controllerContact.js
@@ -19,7 +19,11 @@ const getContact = (req,res) => {
 const createContact = (req,res) => {
     console.log("The request body is", req.body)
     const { name, email, phone } = req.body; // object destructuring 
-    res.status(200).json({message: "Create contact"});
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory !");     
+    }   
+    res.status(201).json({message: "Create contact"});
 };
 
 //@desc Update a contact
