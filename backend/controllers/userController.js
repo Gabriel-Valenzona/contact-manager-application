24de2599b204e2e11cdab68498f804ed31ae508b
Gabriel Valenzona <gabrielvalenzona6@gmail.com>
2025-06-20 10:55:21 -0500

    working on bcrypt and JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 //@desc Register a user
 //@access public
 //@route POST /api/users/register
@@ -42,6 +44,35 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access public
 //@route POST /api/users/login
 const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+
+    const user = await User.findOne({email}); // finds { email: email } and matches accordingly
+
+    // compare password with hashedPassword
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+        {
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        }, 
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1m"}
+        );
+
+        res.status(200).json({accessToken});
+    }
+    else {
+        res.status(401); // throw error if issue in credentials
+        throw new Error("Email or password is not valid");
+    }    
+
     res.json( {message: "Login the user"});
 });
 
